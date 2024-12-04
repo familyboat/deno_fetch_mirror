eec2f965d397863179138e8d28a3b4e8a7b62322
familyboat <2015301110129@whu.edu.cn>
2024-12-04 13:26:18 +0800

    fix typo and prepare for publish

--- util_test.ts
+++ util_test.ts
@@ -3,11 +3,11 @@ import { getName } from "./util.ts";
 
 Deno.test(function addTest() {
   const downloadUrl =
-    `https://github.com/denoland/deno/releases/download/v2.1.2deno-x86_64-unknown-linux-gnu.zip`;
+    `https://github.com/denoland/deno/releases/download/v2.1.2/deno-x86_64-unknown-linux-gnu.zip`;
 
   const { archiveName, denoName } = getName(downloadUrl);
 
-  assertEquals(archiveName, "v2.1.2deno-x86_64-unknown-linux-gnu.zip");
+  assertEquals(archiveName, "deno-x86_64-unknown-linux-gnu.zip");
 
   assertEquals(denoName, "deno");
 });
